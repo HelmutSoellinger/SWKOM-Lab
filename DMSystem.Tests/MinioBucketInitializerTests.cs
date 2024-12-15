06631fbea21771de7255a30b9bcfe8df83b5d72e
your name <your mail@technikum-wien.at>
2024-12-15 23:01:21 +0100

    bucketinitializerTest + curlscript(+test.pdfs)

---
+++ DMSystem.Tests/MinioBucketInitializerTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using DMSystem.Minio;
+
+namespace DMSystem.Tests
+{
+    public class MinioBucketInitializerTests
+    {
+        [Fact]
+        public async Task StartAsync_ShouldInitializeBucketSuccessfully()
+        {
+            // Arrange
+            var mockFileStorageService = new Mock<IMinioFileStorageService>();
+            var initializer = new MinioBucketInitializer(mockFileStorageService.Object);
+
+            // Act & Assert
+            await Record.ExceptionAsync(async () => await initializer.StartAsync(default));
+            mockFileStorageService.Verify(m => m.InitializeBucketAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task StartAsync_ShouldLogErrorIfInitializationFails()
+        {
+            // Arrange
+            var mockFileStorageService = new Mock<IMinioFileStorageService>();
+            mockFileStorageService.Setup(m => m.InitializeBucketAsync())
+                .Throws(new Exception("Initialization failed"));
+            var initializer = new MinioBucketInitializer(mockFileStorageService.Object);
+
+            // Act & Assert
+            await Record.ExceptionAsync(async () => await initializer.StartAsync(default));
+        }
+
+        [Fact]
+        public async Task StopAsync_ShouldCompleteWithoutThrowing()
+        {
+            // Arrange
+            var initializer = new MinioBucketInitializer(Mock.Of<IMinioFileStorageService>());
+
+            // Act & Assert
+            await Record.ExceptionAsync(async () => await initializer.StopAsync(default));
+        }
+    }
+}
